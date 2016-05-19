63245619dfc0c25f656164d46526eed03ca8c1c3
yodaaa <01ewell0@gmail.com>
2016-05-19 14:48:05 +0900

    Initial Commit

---
+++ tracking_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tracking_sample
+//
+//  Created by 依田真明 on 2016/05/19.
+//  Copyright © 2016年 MasaakiYoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
